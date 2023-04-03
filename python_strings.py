d1e686cd018361d06ca9d6e717bab2d0af2bc483
richardclinton <richardclinton42@gmail.com>
2023-04-03 12:16:44 +0300

    string definition

---
+++ python_strings.py
@@ -0,0 +1,2 @@
+# Strings are immutable sequence object, with each character representing an element in the sequence.
+# Strings, being immutable, do not change the instance; each method simply returns a value.
