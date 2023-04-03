069c4d3f08bf89bfd5f1cf13e2d63770d2e142b5
richardclinton <richardclinton42@gmail.com>
2023-04-03 10:48:23 +0300

    three important characteristics of data structures and algorithm

--- definition.py
+++ definition.py
@@ -1,2 +1,7 @@
 # Data structures and algorithm
 # They are systematic way of storing and organizing data in software so that it can be used efficiently.
+# They are the main building block from which  complex system is built. Having an understanding of these foundation
+# concept is extremely important in software design and this involves the following three important characteristics.
+#   1. How algorithms manipulates infromation contained within the data structures.
+#   2. How data is arranged in memory.
+#   3. What the performance characteristics of particular data structures are.
