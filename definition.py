5cce3899a9aa34aea715b1d9d18e4f6bbccfbc8f
richardclinton <richardclinton42@gmail.com>
2023-04-03 10:59:54 +0300

    python for data

--- definition.py
+++ definition.py
@@ -5,3 +5,16 @@
 #   1. How algorithms manipulates infromation contained within the data structures.
 #   2. How data is arranged in memory.
 #   3. What the performance characteristics of particular data structures are.
+
+
+# Python for Data
+# Python has several built-in data structures, including lists, dictionaries and sets, which we used to
+# build customized objects. In addition, there are a number of internal libraries, such as collections
+# and math object which allow us to create more advanced data structures as well as perform calculations on
+# those structures.
+# Finally, there are the external libraries such as those found in SciPy packages. These allow us to perform 
+# a range of advanced data tasks such as logistics and linear regression, visualization and mathematical calculations,
+# such as operation on matrices and vectors.
+# External libraries can be useful for an out-of-the-box solution. However we must also be aware that there is often
+# a performance penalty compared to building customized objects from ground up. By learing how to code these objects 
+# ourselves, we can target them to specific tasks, making them more efficient.
