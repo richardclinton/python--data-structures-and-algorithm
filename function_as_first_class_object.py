87393824df155d5b6410c7a1319d487626dfb728
richardclinton <richardclinton42@gmail.com>
2023-04-04 21:02:05 +0300

    include user-defined object in other objects

--- function_as_first_class_object.py
+++ function_as_first_class_object.py
@@ -6,3 +6,19 @@
 #  2. Assigned as variable or in a data structure
 #  3. Passed as argument to a function
 #  4. Returned as result of a function
+
+# Example
+
+
+def greeting(language):
+    if language == 'eng':
+        return 'hello world'
+    if language == 'fr':
+        return 'Bonjour le monde'
+    else:
+        return 'language not supported'
+    
+# Since user-defined functions are objects, we can do things such as include them 
+# in other objects, such as lists.
+l = [greeting('eng'),greeting('fr'),greeting('ger')]
+print(l[1])
