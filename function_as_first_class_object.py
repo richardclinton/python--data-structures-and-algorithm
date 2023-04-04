238c00d95cbdc8627d67dd4096cd9f1ed253eb4c
richardclinton <richardclinton42@gmail.com>
2023-04-04 20:52:19 +0300

    first class object definition

---
+++ function_as_first_class_object.py
@@ -0,0 +1,8 @@
+# In python, it is not only data types that are treated as objects.
+# Both functions and classes are what are known as first class objects, allowing
+# them to be manipulated in the same ways as built-in data types.
+# By definition, first class objects are
+#  1. Created at runtime
+#  2. Assigned as variable or in a data structure
+#  3. Passed as argument to a function
+#  4. Returned as result of a function
