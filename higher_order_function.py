8e68e3740c6e41f2c76f2a2889a4871a61cb29a4
richardclinton <richardclinton42@gmail.com>
2023-04-05 09:31:42 +0300

    high order function definition

---
+++ higher_order_function.py
@@ -0,0 +1,7 @@
+# Higher order functions
+# Functions that take other functions as arguments, or that return functions
+# Python 3 contains two built-in higher order function, `filter()` and `map()`
+# In python three this functions return an iterator, making them much more efficient.
+# The map() function provides an easy way to transform each item into an iterable object.
+# For example, here is an efficient, compact way to perform an operation on sequence.
+# Note the use of the `lambda` anonymous function.
