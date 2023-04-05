f5e8752ca4c7aa33a0520eca9cc47535885d9d1d
richardclinton <richardclinton42@gmail.com>
2023-04-05 09:38:56 +0300

    map function implementation

--- higher_order_function.py
+++ higher_order_function.py
@@ -5,3 +5,7 @@
 # The map() function provides an easy way to transform each item into an iterable object.
 # For example, here is an efficient, compact way to perform an operation on sequence.
 # Note the use of the `lambda` anonymous function.
+
+lst = [1,2,3,4]
+for item in map(lambda n: n*2, lst):
+    print(item)
