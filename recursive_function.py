461b296507fd62dd5c7e6f7a022bc014dfcf934c
richardclinton <richardclinton42@gmail.com>
2023-04-05 10:21:55 +0300

    recursion definition

---
+++ recursive_function.py
@@ -0,0 +1,5 @@
+# Recursion
+# Function takes one or more calls to itself during execution.
+# Loop iterations and recursion are different in the sense that loops execute
+# statements repeatedly through a boolean condition or through a series of elements,whereas recursion
+# repeatedly calls a function.
