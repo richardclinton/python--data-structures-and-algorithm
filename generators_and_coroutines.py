cde9580d6d4b1c05bfb6975ae0be3fbe685e4fb0
richardclinton <richardclinton42@gmail.com>
2023-04-06 20:01:35 +0300

    generator definition

---
+++ generators_and_coroutines.py
@@ -0,0 +1,6 @@
+# Generators
+# We can create functions that do not just return one result but rather an
+# entire sequence of results, by using the yield statement.
+# Python contains generator functions, which are an easy way to create iterators and
+# are especially useful as a replacement of unworkably long list.
+# A generator yields items rather than build lists. 
