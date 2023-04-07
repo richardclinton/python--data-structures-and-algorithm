8de50f2d98862d54bbfd3368b4afad40d0e93d27
richardclinton <richardclinton42@gmail.com>
2023-04-07 19:53:06 +0300

    inheritance definiton

---
+++ python_inheritance.py
@@ -0,0 +1,10 @@
+# Inheritance
+# Is  one of most powerful features of object-oriented programming
+# It allows us to inherit the functionality from other classes. It is possible to
+# create a new class that modifies the behaviour of an existing class through inheritance.
+# Inheritance means that if an object of one class is created by inheriting another class,
+# the the object would have all functionality, methods and variables of both the classes;
+# that is, the parent class and new class. The existing class from which we inherit the functionalities
+# is called parent/base class, and the new class is called derived/child class.
+# Inheritance in Python is done by passing the inherited class as an argument in the class definition
+# It is often used to modify the behaviour of existing methods.
