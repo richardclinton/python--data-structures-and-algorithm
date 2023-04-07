31fe36cac118f3f06c39a59ffde2a71fee888b73
richardclinton <richardclinton42@gmail.com>
2023-04-07 18:56:30 +0300

    class definition

---
+++ classess_and_objects.py
@@ -0,0 +1,7 @@
+# Classes
+# Are a way to create new kinds of objects, and they are central to object-oriented programming.
+# A class defines a set of attributes that are shared across instances of that class.Typically, classess are sets
+# of functions, variables and properties.
+# The functions defined inside a class are called instance methods. They apply some operations
+# to the class instance by passing an instance of that class as the first argument. This argument is
+# called `self` by convention, but it can be any legal identifier.
