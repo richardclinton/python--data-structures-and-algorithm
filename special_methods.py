463dbcd3f551e47610eebb4c3ce9ebe730e0a3f0
richardclinton <richardclinton42@gmail.com>
2023-04-07 19:42:43 +0300

    special methods implementation

--- special_methods.py
+++ special_methods.py
@@ -13,4 +13,18 @@
 # of built-in types.
 # In the following code we create a class that implements `__repr__()` method. This method creates string
 # representation of our object that is useful for inspection purposes.
+
+class MyClass:
+    def __init__(self, greet):
+        self.greet = greet
+        
+    def __repr__(self):
+        return 'a custom object (%r)' % self.greet
+    
+    # Notice the use the `%r` forma placeholder to return the standard representation of the object
+    # This is useful and best practice because, in this case, it shows us that the `greet` object is
+    # a string indicated by the quation mark
+    
+a = MyClass('Clinton')
+print(a)
  
