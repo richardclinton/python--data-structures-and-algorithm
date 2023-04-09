c61cd44666f9d11d8449eb9e116887101ee65dc4
richardclinton <richardclinton42@gmail.com>
2023-04-09 17:21:44 +0300

    numeric type definition

---
+++ python_numeric-type.py
@@ -0,0 +1,7 @@
+# Number types include the follwoing
+# 1. Integers(int), whole number of unlimited range
+# 2. Floating-point numbers(float)
+# 3. Complex numbers(complex), which are represente by two float numbers 
+# 4. Boolean (bool)
+# Python provides the `int` data type that allows standard arthmetic operators (+,*,- and /) to work on them.
+# The boolean data type has two possible values, `True` and `False`. These values are mapped to 0 and 1, respectively.
