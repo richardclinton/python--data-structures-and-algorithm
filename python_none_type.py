a6af9dbf031fd00589b22135a218ba19a1317f78
richardclinton <richardclinton42@gmail.com>
2023-04-09 17:11:07 +0300

    None data type

---
+++ python_none_type.py
@@ -0,0 +1,7 @@
+# None
+# It is immutable type.
+# It is used to show the absence of a value
+# Objects return None where there is actually nothing to return.
+# It is also returned by `False` Boolean expression.
+# It is often used as default value in function arguments to detect whether a function call
+# has passed a value or not. 
