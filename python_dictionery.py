58188d1c9a19a890e5b1430bb6e529aa20898895
richardclinton <richardclinton42@gmail.com>
2023-04-12 12:35:31 +0300

    ductionary sorted method

--- python_dictionery.py
+++ python_dictionery.py
@@ -126,4 +126,60 @@ print(d,end='\n\n')
 print("values method")
 print(d)
 print(d.values())
-print(type(d.values()))
+print(type(d.values()),end='\n\n')
+
+
+# Sorting dictionaries
+# If you want to do a simple sort on either the keys or values of a dictionary, we can do as folloes.
+print("sorted method")
+d = {'one':1,'two':2,'three':3,'four':4,'five':5,'six':6}
+print(d)
+print(sorted(list(d)))
+print(sorted(list(d.values())))
+
+# The `sorted()` method has two optional arguments that are of interest: `key` and `reverse`. The `key` argument
+# has nothing to do with the dictionary keys, but rather is a way of passing a function to the sort algorithm
+# to determine the sort order. For example, in the following code, we use the `__getitem__` special method
+# to sort the dictionery keys according to the dictionary values.
+print(sorted(list(d),key=d.__getitem__))
+
+# Essentially, what the preceding code is doing is, for every key in d, it uses the corresponding value to sort.
+# We can also sort the values according to the sorted order of the dictionary keys. However, since dictionary 
+# do not have a method to return a key by using its value, the equivalent of the `list.index` method for lists,
+# using the optional key argument to do this is a little tricky. An alternative approach is to use a list comprehension
+# as following example demonstrates.
+value_lst = [value for (value, key) in sorted(d.items())]
+print(value_lst)
+
+# The `sorted()` method also has an optional reverse argument, and unsuprisingly this does exactly
+# waht it says, reverses the order of the sorted list, as in code below.
+print("sorted without reversing",sorted(list(d),key=d.__getitem__))
+print("sorted with reversing", sorted(list(d),key=d.__getitem__,reverse=True))
+
+# Now, let's say we are given the following dictionary, with English words
+# as keys and French words as values. Our task is to place the string values
+# in the correct numerical order:
+d2 = {'one':'uno','two':'deux','three':'trois','four':'quatre','five':'cinq','six':'six'}
+# Of course, when we print this dictionary out, it will be unlikely to print
+# in the correct order. Because all keys and values are strings, we have no
+# context for numerical ordering. To place these items in correct order, we
+# need to use the first dictionary we created, mapping words to numerals as
+# a way to order our English to French dictionary:
+print(sorted(d2,key=d.__getitem__))
+
+# Notice we are using the values of the first dictionary d,to sort the keys ofthe second dictionary,d2.
+# Since our keys in both dictionaries are thesame, we can use a list comprehension to sort the values of 
+# the French to English dictionary:
+lst_value = [d2[i] for i in sorted(d2,key=d.__getitem__)]
+print(lst_value)
+
+# We can, of course, define our own custom method that we can use as the
+# key argument to the sorted method. For example, here we define a
+# function that simply returns the last letter of a string:
+def corder(string):
+    return (string[len(string)-1])
+
+# We can then use this as the key to our sorted function to sort each
+# element by its last letter:
+lst_sorted = sorted(d2.values(), key=corder)
+print(lst_sorted)
