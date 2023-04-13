6231df60152b5feb98e28f766db2a28f01206352
richardclinton <richardclinton42@gmail.com>
2023-04-13 11:19:21 +0300

    set len, copy, diffrence and intersection method

--- python_sets.py
+++ python_sets.py
@@ -8,3 +8,26 @@
 # Set are created using comma-separated values within curly braces. By the way, we can not create an empty 
 # set using `a={}`, because this will create a dictionary. To create an empty set, we write either `a=set()`
 # or `a=frozenset()`.
+
+# Methods and operations of sets
+# 1. len(a) => Provides the total number of element in the `a` set.
+a = {1,2,3,4,5}
+print("len method")
+print(len(a))
+print(type(a),end='\n\n')
+# 2. copy(a) => Provides another copy of the `a` set.
+print("copy method")
+print(a)
+b = a.copy()
+print(b,end='\n\n')
+# 3. a.difference(t) => Provides a set of elements that are in the `a` set but not in `t`
+t = {2,4,6,8,10}
+print("difference method")
+print('a',a)
+print('t',t)
+print(a.difference(t),end='\n\n')
+# 4. a.intersection(t) => Provides a set of elements that are in both set,`a` and `t`
+print("intersection method")
+print('a',a)
+print('t',t)
+print(a.intersection(t))
