47eedc691bcfe62b953adc5aae208f8c5e9e1619
richardclinton <richardclinton42@gmail.com>
2023-04-14 19:35:16 +0300

    ADTs and their extension libraries

---
+++ data-structure/module_for_data_structures_and_algorithm.py
@@ -0,0 +1,9 @@
+# In addtion to the built-in data type,there are several Python modules that we can use
+# to extend the built-in types and functions. In many cases these, these Python modules
+# offer efficiency and programming advantages that allow us to simplify our code.
+# The built-in data type, we have looked so far,strings,lists,dictionaries,tuples,sets as well as
+# the decimal and fraction modules. They are often described by the term Abstarct Data Types (ADTs)
+# ADT(s) can be considered mathematical specifications for the set of operataions that can be
+# performed on data. They are defined by their behaviour rather than their implementation. In addition
+# to the ADTs that we have looked at, there are several Python libraries that provide extensions to the
+# built-in data-types.
