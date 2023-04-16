3b0220a46360ae9e4307499e1ccba90cbbfcb284
richardclinton <richardclinton42@gmail.com>
2023-04-16 11:47:07 +0300

    Counter object most_common and substract method,difference between counter and dictionary

--- data-structure/collections_counter_object.py
+++ data-structure/collections_counter_object.py
@@ -10,7 +10,7 @@ c2 = Counter({'a':1,'c':2,'e':3})
 c3 = Counter(a=1,c=1,e=3)
 print('c1',c1)
 print('c2',c2)
-print('c3',c3)
+print('c3',c3, end='\n\n')
 
 
 # We can also create an empty counter object and populate it passing its `update` method
@@ -25,3 +25,29 @@ ct.update({'a':3}) # update the value of `a`
 print("ct.update({'a':3})",ct)
 for item in ct:
     print("%s:%d" % (item,ct[item]))
+print(end='\n\n')   
+    
+# The most notable difference between counter objects and dictionaries is that counter
+# objects return a zero count for missing items rather than raising a key error. We can create
+# an iterator out of a `Counter` object by using its `elements()` method. This return an iterator
+# where counts below one are not included and the order is not guaranteed. In the following code,
+# we performs some update, creates an iterator from `Counter` elements and use `sorted()` to the
+# keys alphabetically.
+print(ct)
+print('ct[x]',ct['x'])
+ct.update({'a':-3,'b':-2,'e':2})
+print("ct.update({'a':-3,'b':-2,'e':2})", ct)
+print("sorted(ct.elements())",sorted(ct.elements()),end='\n\n')
+
+# Tow other Counter methods worth mentioning are `most_common()` and `substract()`.
+# The most common method takes a positive interger argument that determines the number of 
+# most common elements to return. Elements are returned as a list of (key,value) tuples.
+# The substract method works exactly like update except,instead of adding values, it substract them.
+print('most_common method')
+print('ct',ct)
+print('ct.most_common()', ct.most_common(),end='\n\n')
+print('substract method')
+print('ct',ct)
+ct.subtract({'e':2})
+print("ct.subtract({'e':2})",ct)
+
